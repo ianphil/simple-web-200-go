5b1ea2d80bbbec6109fe844b3e6ac6f31c81c93f
Ian Philpot <ian.philpot@microsoft.com>
2018-09-30 16:35:58 -0400

    should do it

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}

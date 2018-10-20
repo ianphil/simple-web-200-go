c12a54d01ad4dafe58a8fb7cb6cc420008a7b0ee
Ian Philpot <ian.philpot@microsoft.com>
2018-10-20 05:39:25 -0400

    sample code

---
+++ sample/this.go
@@ -0,0 +1,15 @@
+package sample
+
+import (
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
